455bdae6fd27f522babdf33701c6c2ab792d0791
Khristine Sagum <tinsagum11@gmail.com>
2022-03-07 16:30:33 +0400

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var cacheName = 'petstore-v1';
 var cacheFiles = [
     'index.html',
-    'product.js',
+    'products.js',
     'petstore.webmanifest',
     'images/catfood.png',
     'images/yarn.jpg',
